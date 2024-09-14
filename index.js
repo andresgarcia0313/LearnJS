2ba1afbdda9dff7e58c4e5c9386e61f92c9b6fae
Andrés Eduardo García Márquez <andresgarcia0313@gmail.com>
2024-09-13 20:05:05 -0500

    cambios nuevos ;)

--- index.js
+++ index.js
@@ -3,37 +3,4 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("<h1>¡Hola Mundo desde Express en Netlify Functions! 🌍</h1>");
 });
-// Esto es un comentario 😊
-// Un programa básico en JavaScript que imprime un saludo 🎉
-// 01 imprimir un saludo en la consola 🎉
-console.log("¡Hola, mundo! Bienvenido a JavaScript 🚀");
-
-// 02 Declaración de variables y tipos de datos 😊
-let nombre = "Andrés Eduardo"; // String (Cadena de texto)
-let edad = 36; // Number (Número)
-let esIngeniero = true; // Boolean (Booleano)
-let auto = null; // Null (Valor nulo)
-let pareja; // Undefined (Indefinido)
-let hobbies = ["Leer", "Programar", "Viajar"]; // Array (Arreglo)
-// Object (Objeto)
-let direccion = {
-  calle: "Cr 87c # 50b-03",
-  ciudad: "Bogotá",
-  pais: "Colombia",
-};
-
-// Mostrar los valores en la consola 🎉
-console.log("Nombre:", nombre);
-console.log("Edad:", edad);
-console.log("Es ingeniero:", esIngeniero);
-console.log("Auto:", auto);
-console.log("Pareja:", pareja);
-console.log("Hobbies:", hobbies);
-console.log("Hobbies:", hobbies[0]);
-console.log("Hobbies:", hobbies[2]);
-console.log("Dirección:", direccion);
-console.log("Dirección:", direccion.calle);
-console.log("Dirección:", direccion.ciudad);
-console.log("Dirección:", direccion.pais);
-// Exporta la función para Netlify
 module.exports.handler = serverless(app);
